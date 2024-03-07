71e06b70936a5e217651a19ac238c1d61ca8f4a7
peter staal <peterstaal@MBP-van-peter.home>
2024-03-07 12:37:47 +0100

    exercise 3.9-3.11

---
+++ persons/README.md
@@ -0,0 +1,1 @@
+This app runs on https://persons-ps.fly.dev/
--- persons/index.js
+++ persons/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
+
 const morgan = require("morgan");
+
+app.use(express.static("dist"));
 
 app.use(express.json());
 morgan.token("type", function (req, res) {
@@ -95,7 +101,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
